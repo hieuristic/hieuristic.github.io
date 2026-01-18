e1dcba8e1ca808423b1214dd3dc1415ffd187ece
Charlie <hieu1052k3@gmail.com>
2026-01-18 22:27:43 +0700

    reset

--- scripts/propagate_template.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from bs4 import BeautifulSoup as bs
-
-top_dir = Path(__file__).parent.parent
-with open(top_dir / "templates/sandwich.html", "r") as f:
-    soup = bs(f.read(), "html.parser")
-
-print(soup)
